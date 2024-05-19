0e2ace8379e724c157bc38cbcf6393b8654ed5d9
Belikeawuley <151824430+Belikeawuley@users.noreply.github.com>
2024-05-19 12:46:28 +0000

    Update arrayManipulation.js
    Add function formatArrayStrings to arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,13 @@
 // Task 1: processArray
-const processArray = (arr) => {
+const processArray = (arr) ==> {
   return arr.map((num) => (num % 2 === 0 ? num * num : num * 3));
 };
+
+// Task 2: formatArrayStrings
+const formatArrayStrings = (strArr, numArr) ==> {
+  return strArr.map((str, index) ==>
+     numArr[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase()
+ );
+};
+
+module.exports = { processArray, formatArrayStrings };
